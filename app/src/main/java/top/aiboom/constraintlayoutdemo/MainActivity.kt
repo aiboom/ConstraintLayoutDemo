04cb2b51587fb8bd94d695abd06718a97246790b
langsky <langsky001@hotmail.com>
2018-06-12 00:24:14 +0800

    sync

--- app/src/main/java/top/aiboom/constraintlayoutdemo/MainActivity.kt
+++ app/src/main/java/top/aiboom/constraintlayoutdemo/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.example_4)
+        setContentView(R.layout.example_attributes_panel)
     }
 }
